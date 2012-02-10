7fef993685a9365f8d09218d19026e225e1cf081
Rajat Mittal <rajat@rockmelt.com>
2012-02-10 00:26:57 -0800

    Test suite using Jasmine.

---
+++ spec/PromiseSpec.js
@@ -0,0 +1,48 @@
+function async() {
+  var dfd = new Deferred();
+  window.setTimeout(function() {
+    dfd.resolve('Yay');
+  }, 1000);
+  return dfd.promise();
+}
+
+describe('Promises',function(){
+	var promise;
+	//Create a promise before each spec
+	beforeEach(function(){
+		promise = async();
+	});
+	
+	it("Single resolution",function(){
+		var result;
+		promise.done(function(res){
+			result = res;
+		});
+		waits(1000);
+		runs(function(){
+			expect(result).toEqual('Yay');
+		});
+	});
+	
+	it("Multiple resolution",function(){
+		var result1,result2;
+		promise.done([
+			function(res){
+				result1 = res + 1;
+			},
+			function(res){
+				result2 = res + 2;
+			}
+		]);
+		
+		waits(1000);
+		runs(function(){
+			expect(result1+result2).toEqual('Yay1Yay2');
+		});
+	});
+	// 
+	// var p = Promise.when(async(),{a:5});
+	// p.then(function(result){
+	//     console.log(result);
+	// });
+});
